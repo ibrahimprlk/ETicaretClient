17f55ad87c19f6fe06dfde21e25d7c14da5627f5
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2025-04-23 22:47:21 +0300

    material kütüphanesi eklendi

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -15,12 +15,13 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatListModule
   ],
   exports:[
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent   
   ]
 })
 export class ComponentsModule { }
