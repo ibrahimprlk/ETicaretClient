d20cc76c70d8e093dcca1b60949583ba451ab511
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2025-04-24 00:18:01 +0300

    alertify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from './services/admin/alertify.service';
 declare var $:any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ETicaretClient';
+
+  constructor(private alertify:AlertifyService){}
+
+  ngOnInit(): void {
+    this.alertify.message("Hello World!",{messageType:MessageType.Warning,position:Position.BottomCenter,delay:5})
+  }
+  
 
 }
 // $(document).ready(()=>{
