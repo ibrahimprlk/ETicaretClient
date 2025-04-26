4cf351c798fd3dee7831a888d5c2b4bdcd67e563
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2025-04-26 14:55:09 +0300

    Dialog Sayfaların Merkezileştirilmesi

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -14,14 +14,14 @@ import { DeleteDirective } from '../../../directives/admin/delete.directive';
 import {MatDialogModule} from '@angular/material/dialog';
 import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
 import { FileUploadModule } from "../../../services/common/file-upload/file-upload.module";
+import { DialogModule } from '../../../dialogs/dialog.module';
 
 @NgModule({
   declarations: [
     ProductsComponent,
     CreateComponent,
     ListComponent,
-    DeleteDirective,
-    DeleteDialogComponent
+    DeleteDirective
   ],
   imports: [
     CommonModule,
@@ -34,7 +34,7 @@ import { FileUploadModule } from "../../../services/common/file-upload/file-uplo
     MatButtonModule,
     MatTableModule,
     MatPaginatorModule,
-    MatDialogModule,
+    DialogModule,
     FileUploadModule
 ]
 })
